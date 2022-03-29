e07d6938b4984789eb574e30bfe644bad110abfb
bekzod-fayzikuloff <fayzikuloff.b@gmail.com>
2022-03-29 12:40:17 +0300

    Define models into admin panel

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from .models import Post, Comment, Album, Photo, Todo
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "user"]
+    list_filter = ["title", "user"]
+    search_fields = ["title", "user__username"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["name", "email"]
+    list_filter = ["name", "email"]
+    search_fields = ["name", "email"]
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ["user", "title"]
+    search_fields = ["user__username", "title"]
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ["album", "title"]
+    search_fields = ["album__title", "title", "album__user__username"]
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ["user", "title"]
+    search_fields = ["user__username", "title"]
